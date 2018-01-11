af7b4ba3b770adf4d65f13ecf850cdcef08207f3
Robert Lei <mai.lei@edu.chat>
2018-01-11 03:51:56 -0500

    no message

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
